b876ab488eea67c99a3bdae3e76513095717b4f6
zacharykogan <86493264+zacharykogan@users.noreply.github.com>
2021-07-20 19:04:07 -0400

    disabled button after it has been clicked

--- app/auth/events.js
+++ app/auth/events.js
@@ -45,6 +45,7 @@ const onPlay = function(event) {
   let cellIndex = $(event.target).data('cell-index')
   let tie = false
   let win = false
+  $('#cell' + cellIndex + ' button').prop('disabled', true);
   
   win = ui.didAnyoneWin(cellIndex)
   if (!win) {
